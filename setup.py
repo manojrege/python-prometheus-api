67a6ec3b240b8ccb42441783bc740896f9053169
manojrege <manoj.rege@emnify.com>
2019-07-20 16:45:12 +0200

    Add package data

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-from distutils.core import setup
+import  setuptools
 
-setup(
+setuptools.setup(
     name='python-prometheus-api',
-    version='0.1',
+    version='0.1.3',
     author='Manoj R. Rege',
     author_email='rege.manoj@gmail.com',
     url='https://github.com/manojrege/python-prometheus-api',
     description='A Python wrapper for Prometheus HTTP API',
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     license=open('LICENSE').read(),
-    packages=[
-        'promapi'
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "LICENSE :: OSI APPROVED :: BSD LICENSE",
     ],
+    package_data = {'promapi': ['data/config.yml']},
+    include_package_data = True,
 )
